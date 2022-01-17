b20cdcc8bb8fc8d6dbb54b28379ebf1999d94f50
Sapan Patibandha <sapan.patibandha@contis.com>
2022-01-17 15:09:08 +0530

    Add project files.

---
+++ Common.Logging/SeriLogger.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace Common.Logging
+{
+
+    /*
+     * https://github.com/shazforiot/Elasticsearch-kibana-docker-compose-single-node
+     * 
+     * 
+     * 
+     * https://www.elastic.co/guide/en/elasticsearch/reference/7.5/docker.html
+     * https://www.elastic.co/guide/en/kibana/current/docker.html
+     * 
+     * 
+     * 
+     * http://localhost:9200/
+     * 
+     * docker network create elastic
+     * docker pull docker.elastic.co/elasticsearch/elasticsearch:7.16.3
+     * docker run --name es01-test --net elastic -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.16.3
+     * 
+     * 
+     * docker pull docker.elastic.co/kibana/kibana:7.16.3
+     * docker run --name kib01-test --net elastic -p 127.0.0.1:5601:5601 -e "ELASTICSEARCH_HOSTS=http://es01-test:9200" docker.elastic.co/kibana/kibana:7.16.3
+     * 
+     * 
+     * http://localhost:5601
+     * 
+     * 
+     */
+    public static class SeriLogger
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+           (context, configuration) =>
+           {
+               var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+
+               configuration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Debug()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(new Uri(elasticUri))
+                        {
+                            IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                            AutoRegisterTemplate = true,
+                            NumberOfShards = 2,
+                            NumberOfReplicas = 1
+                        })
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                    .ReadFrom.Configuration(context.Configuration);
+           };
+    }
+}
